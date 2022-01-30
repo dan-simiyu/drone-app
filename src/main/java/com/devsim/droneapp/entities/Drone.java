e6c7e72bac0638e2d498bf612f34e5033c7fc269
dan-simiyu <63334852+dan-simiyu@users.noreply.github.com>
2022-01-30 09:37:45 +0300

    updated the entities

--- src/main/java/com/devsim/droneapp/entities/Drone.java
+++ src/main/java/com/devsim/droneapp/entities/Drone.java
@@ -2,18 +2,49 @@ package com.devsim.droneapp.entities;
 
 import com.devsim.droneapp.enums.Model;
 import com.devsim.droneapp.enums.State;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "drones")
 public class Drone {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @Column(unique = true)
+    @NotBlank(message = "Serial number is mandatory")
     private String serialNumber;
+
+    @Column()
+    @Enumerated(EnumType.STRING)
     private Model model;
+
+    @Column()
     private int weightLimit;
+
+    @Column()
+    @Range(min = 0, max = 100)
     private int batteryCapacity;
+
+    @Column()
+    @Enumerated(EnumType.STRING)
     private State state;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "drone", fetch = FetchType.EAGER)
     private List<Medication> medication;
+
+
 }
--- src/main/java/com/devsim/droneapp/entities/Medication.java
+++ src/main/java/com/devsim/droneapp/entities/Medication.java
@@ -1,4 +1,37 @@
 package com.devsim.droneapp.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "medications")
 public class Medication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int weight;
+
+    @Column
+    private String code;
+
+    @Column
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name="drone_id")
+    @JsonBackReference
+    private Drone drone;
 }
