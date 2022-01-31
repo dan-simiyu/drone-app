4c8323aa5da248a01d037a3bf99598ce642a120c
dan-simiyu <63334852+dan-simiyu@users.noreply.github.com>
2022-01-31 08:34:55 +0300

    modified drone and medication repositories

--- src/main/java/com/devsim/droneapp/repositories/DroneRepository.java
+++ src/main/java/com/devsim/droneapp/repositories/DroneRepository.java
@@ -2,6 +2,8 @@ package com.devsim.droneapp.repositories;
 
 import com.devsim.droneapp.entities.Drone;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DroneRepository extends CrudRepository<Drone, Long> {
 }
--- src/main/java/com/devsim/droneapp/repositories/MedicationRepository.java
+++ src/main/java/com/devsim/droneapp/repositories/MedicationRepository.java
@@ -2,6 +2,8 @@ package com.devsim.droneapp.repositories;
 
 import com.devsim.droneapp.entities.Medication;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicationRepository extends CrudRepository<Medication, Long> {
 }
